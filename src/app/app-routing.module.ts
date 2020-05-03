d3e09605a6e0984ed3446f6195bc41bf1603ec03
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-05-03 23:23:20 +1000

    Page 'Common Question' added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
 import {LoginViewComponent} from './views/login-view/login-view.component';
+import {CommonQuestionsComponent} from './views/common-questions/common-questions.component';
 
 const routes: Routes = [
   { path: '', component: MainLayoutComponent, children: [
       { path: '', redirectTo: '/login', pathMatch: 'full' },
-      { path: 'login', component: LoginViewComponent }
+      { path: 'login', component: LoginViewComponent },
+      { path: 'common-questions', component: CommonQuestionsComponent }
   ]},
   {
     path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { LoginViewComponent } from './views/login-view/login-view.component';
+import { CommonQuestionsComponent } from './views/common-questions/common-questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    LoginViewComponent
+    LoginViewComponent,
+    CommonQuestionsComponent
   ],
   imports: [
     BrowserModule,
